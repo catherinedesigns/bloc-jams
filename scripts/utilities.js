4bc5f390efbf7a66c5246b75fc6c3b2e99e1685a
Catherine <catherine.s.mu@gmail.com>
2017-10-04 16:43:31 -0700

    ckpt 22 hw

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,10 +1,4 @@
-// var forEach = function(array) {
-//     for (var i = 0; i < array.length; i++) {
-//       console.log(array[i])
-//     }
-// };
-
-//correct
+//Checkpoint 22 homework
 function forEach(array, callback) {
   for (var i = 0; i < array.length; i++) {
     callback(array[i]);
