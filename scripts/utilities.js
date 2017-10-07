21d63413d69769a59f0f35339e04cbf2317b80e8
Catherine <catherine.s.mu@gmail.com>
2017-10-07 11:58:15 -0700

    delete comments

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,9 +1,3 @@
-// var forEach = function(array) {
-//     for (var i = 0; i < array.length; i++) {
-//       console.log(array[i])
-//     }
-// };
-
 //correct
 function forEach(array, callback) {
   for (var i = 0; i < array.length; i++) {
