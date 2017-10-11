18e5ef990deb97e09f095bc9ee760e27946f213c
Catherine <catherine.s.mu@gmail.com>
2017-10-10 17:18:34 -0700

    add jQuery for landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,58 +1,33 @@
-//checkpoint 21 homework
-// var pointsArray = document.getElementsByClassName('point');
-//
-//  var animatePoints = function(points) {
-//
-//     var revealPoint = function(index) {
-//       points[index].style.opacity = 1;
-//       points[index].style.transform = "scaleX(1) translateY(0)";
-//       points[index].style.msTransform = "scaleX(1) translateY(0)";
-//       points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-//     };
-//
-//       for (var i = 0; i < points.length; i++) {
-//       revealPoint(i);
-//     }
-// };
-//
-//   window.onload = function() {
-//     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-//     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-//
-//     window.addEventListener('scroll', function(event) {
-//       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-//              animatePoints(pointsArray);
-//          }
-//   });
-// }
-
-
-//checkpoint 22 homework
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function(point) {
-  point.style.opacity = 1;
-  point.style.transform = "scaleX(1) translateY(0)";
-  point.style.msTransform = "scaleX(1) translateY(0)";
-  point.style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
- var animatePoints = function(points) {
-   forEach(points, revealPoint);
+// Checkpoint 28
+// #5
+var animatePoints = function() {
+  var revealPoint = function() {
+        // #7
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    // #6
+     $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+
+
+// Checkpoint 28
+// Make window a jQuery object
+$(window).load(function() {
+      // #1 jQuery's height() method
+       if ($(window).height() > 950) {
+           animatePoints();
      }
-
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-    window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-           animatePoints(pointsArray);
+     // #2 jQuery's height() method
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     // #3
+     $(window).scroll(function(event) {
+     // #4
+      if ($(window).scrollTop() >= scrollDistance) {
+         animatePoints();
          }
      });
-}
+});
